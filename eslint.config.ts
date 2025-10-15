627ffd67e2b3885e571d11fe35498be3f6c6abb3
Ringo37 <yajima.haruhiko@gmail.com>
2025-10-15 15:09:22 +0900

    formatterなどを追加

---
+++ eslint.config.ts
@@ -0,0 +1,69 @@
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: [
+      "build/**",
+      "node_modules/**",
+      ".react-router/**",
+      ".env",
+      "coverage/**",
+    ],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+
+      globals: {
+        window: true,
+        document: true,
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "@typescript-eslint": tsPlugin,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: [".ts", ".tsx"],
+        },
+      },
+    },
+    rules: {
+      "no-undef": "off",
+      "import/order": [
+        "error",
+        {
+          alphabetize: { order: "asc", caseInsensitive: true },
+          groups: ["builtin", "external", "internal", "parent", "sibling"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+];
