9329c77179a141b39bc6e79f6195265769fc6a2b
1F10230157 <s1f102301572@iniad.org>
2025-10-23 10:33:09 +0900

    コンテンツベースフィルタリングのAPI追加

--- model_api/app/main.py
+++ model_api/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from .schemas import TextRequest, ExtractKeywordsResponse
+from fastapi import FastAPI, HTTPException
+from .schemas import TextRequest, ExtractKeywordsResponse, RecommendRequest, RecommendResponse
 from .modules.keyword import extract_keywords_core
+from .modules.recommend import recommend
+
+
 
 app = FastAPI()
 
@@ -23,3 +26,28 @@ def extract_keywords_api(request: TextRequest):
         return {"status": "success", "keywords": keywords}
     except Exception as e:
         return {"status": "error", "message": str(e)}
+    
+
+
+@app.post("/recommend", response_model=RecommendResponse)
+def recommend_api(request: RecommendRequest):
+    # 1. タグ集合作成
+    all_tags = list(
+        set(
+            sum(
+                [k.tags.split(',') for k in request.knowhows] +
+                [l.interest_tags.split(',') for l in request.learners],
+                []
+            )
+        )
+    )
+    learner = next((le for le in request.learners if le.name == request.user_name), None)
+    if learner is None:
+        return {"recommendations": []}
+    result = recommend(
+        [k.dict() for k in request.knowhows],
+        learner.dict(),
+        all_tags,
+        top_n=request.top_n
+    )
+    return {"recommendations": result}
