d14f889c23079084b9f6868add05e5b03a3f9cf7
Ringo37 <yajima.haruhiko@gmail.com>
2025-10-29 15:36:14 +0900

    設定ページを作成

--- app/routes.ts
+++ app/routes.ts
@@ -12,6 +12,8 @@ export default [
   layout("routes/layout.tsx", [
     route("sample", "routes/sample.tsx"),
     route("worklogformat", "routes/worklogformat.tsx"),
+    route("mypage", "routes/user/mypage.tsx"),
+    route("settings", "routes/user/settings.tsx"),
     route("edit-interest-tags", "routes/edit_interest_tags.tsx"),
   ]),
 ] satisfies RouteConfig;
