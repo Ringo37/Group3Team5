9ba73bfc6b387c34afafcbe85eeb8d5ebf5b00ff
Ringo37 <yajima.haruhiko@gmail.com>
2026-01-12 20:23:19 +0900

    fix

--- app/routes/knowhow/explore.tsx
+++ app/routes/knowhow/explore.tsx
@@ -18,7 +18,6 @@ import {
   type LoaderFunctionArgs,
 } from "react-router";
 
-import Footer from "~/components/fotter";
 import { PostCard } from "~/components/postCard";
 import { prisma } from "~/lib/prisma";
 import { getKnowHows, getKnowHowsCount } from "~/models/knowhow.server";
@@ -140,18 +139,8 @@ export default function Explore() {
 
   return (
     <Flex direction="column" minH="100vh" bg="#fdf8f3">
-      <Box
-        flex="1"
-        pb={20}
-        mx="auto"
-        maxWidth="7xl"
-        width={{ base: "95%", md: "90%", lg: "65%" }}
-        px={0}
-      >
+      <Box flex="1" width="100%" pb={20}>
         <Box maxW="7xl" mx="auto" px={{ base: 6, md: 10 }}>
-          <Heading as="h1" size="2xl" textAlign="center" mt={10}>
-            ノウハウを検索
-          </Heading>
           <Form method="get" action=".">
             {/* 検索時、おすすめモードがONなら維持するための隠しフィールド */}
             {isRecommended && (
@@ -160,7 +149,7 @@ export default function Explore() {
 
             <InputGroup
               startElement={<Search size={16} color="gray" />}
-              marginY={10}
+              marginY={5}
             >
               <Input
                 name="search"
@@ -262,7 +251,6 @@ export default function Explore() {
           )}
         </Box>
       </Box>
-      <Footer />
     </Flex>
   );
 }
